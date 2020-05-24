4ed4d830abc94ed8b8b0e7aeeeab884f9c511f8e
Ciremun <Ciremun@yandex.ru>
2020-05-24 15:15:16 +0300

    badges

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let messages = [];
 let max_messages = 10;
+let badges = true;
 const socket = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
 
 async function main () {
@@ -20,22 +21,24 @@ async function main () {
         console.log(event.data);
         if (event.data.startsWith('PING :tmi.twitch.tv')) return socket.send('PONG :tmi.twitch.tv');
         let username = /display-name=(\w+);/.exec(event.data);
-        if (!username) {
-            username = chat_msg.exec(event.data);
-            if (!username) return;
-        }
+        if (!username) return;
         username = username[1];
         let color = /color=(#[A-Fa-f0-9]{6})/.exec(event.data)[1];
         let message = event.data.replace(chat_msg, "");
-        console.log(`${username}: ${message}`);
         if (messages.length >= max_messages) {
             let first = messages.shift();
             first.addEventListener('animationend', () => first.parentNode.removeChild(first));
             first.style.animation = 'fade-out 0.2s forwards';
           }
         let chatmsg = document.createElement('div');
+        let tags = {};
+        event.data.split(';').forEach(x => {x = x.split('='); tags[x[0]] = x[1];});
+        if (badges) {
+            tags.badges = tags.badges.split(',').map(x => x.split('/'));
+            tags.badges.forEach(x => chatmsg.innerHTML += `<img id="badge" src="static/${x[0]}${x[1]}.png">`);
+        }
         chatmsg.id = 'chatmsg';
-        chatmsg.innerHTML = `<span style=color:${color}>${username}</span> ${message}`;
+        chatmsg.innerHTML += `<span style=color:${color}>${username}</span> ${message}`;
         document.querySelector('#chatmsgs').appendChild(chatmsg);
         messages.push(chatmsg);
     }
